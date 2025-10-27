e1e2b695c55b81efdf2a9ceeaa7e293e1b02151b
togethf <1720425326@qq.com>
2025-10-27 20:00:00 +0800

    add tools for exp run in paper

--- double-branch.py
+++ double-branch.py
@@ -728,7 +728,7 @@ if __name__ == "__main__":
             print_per_class_metrics(mp, mr, map50, mf1, p, r, ap, f1, class_ids, n_gt, n_p, class_names=class_names, dataset_len=len(dataset))
         print("===========end evaluation=============")
     else:
-        for i in [0,1,4]:
+        for i in [2]:
             model_single_path = det_pool[i]
             print(f"===========evaluation single yolo[{i}]=============")
             metrics_output = evaluate_single(model_single_path, dataloader, device)
